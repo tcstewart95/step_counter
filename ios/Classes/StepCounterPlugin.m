5f188681a5af0bae189d3056237d8fd1525f58ad
tcstewart95 <tcstewart95@gmail.com>
2019-07-08 12:56:07 -0600

    default query

--- ios/Classes/StepCounterPlugin.m
+++ ios/Classes/StepCounterPlugin.m
@@ -35,15 +35,56 @@
   
 }
 
-- (String)getStepsInIntervals:(int*)startTime :(int*)endTime :(int*)intervals {
+- (String)getStepsInIntervals:(int)startTime :(int)endTime :(int)intervals {
 
 }
 
-- (String)getStepsDuringTime:(int*)startTime :(int*)endTime {
+- (String)getStepsDuringTime:(int)startTime :(int)endTime {
 
 }
 
 - (String)getStepsToday:() {
 
+}
+
+- (String)executeQuery:(NSDate)startTime :(NSDate)endTime {
+  NSCalendar *calendar = [NSCalendar currentCalendar];
+  NSDateComponents *interval = [[NSDateComponents alloc] init];
+  interval.day = 1;
+
+  NSDateComponents *anchorComponents = [calendar components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear
+    fromDate:[NSDate date]];
+  anchorComponents.hour = 0;
+  NSDate *anchorDate = [calendar dateFromComponents:anchorComponents];
+  HKQuantityType *quantityType = [HKObjectType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+
+  // Create the query
+  HKStatisticsCollectionQuery *query = [[HKStatisticsCollectionQuery alloc] initWithQuantityType:quantityType
+    quantitySamplePredicate:nil
+    options:HKStatisticsOptionCumulativeSum
+    anchorDate:anchorDate
+    intervalComponents:interval];
+
+  // Set the results handler
+  query.initialResultsHandler = ^(HKStatisticsCollectionQuery *query, HKStatisticsCollection *results, NSError *error) {
+    if (error) {
+        // Perform proper error handling here
+        NSLog(@"*** An error occurred while calculating the statistics: %@ ***",error.localizedDescription);
+    }
+
+    // Plot the daily step counts over the past 7 days
+    [results enumerateStatisticsFromDate:startDate
+      toDate:endDate
+      withBlock:^(HKStatistics *result, BOOL *stop) {
+        HKQuantity *quantity = result.sumQuantity;
+        if (quantity) {
+            NSDate *date = result.startDate;
+            double value = [quantity doubleValueForUnit:[HKUnit countUnit]];
+            NSLog(@"%@: %f", date, value);
+        }
+    }];
+  };
+
+  [self.healthStore executeQuery:query];
 }
 @end
