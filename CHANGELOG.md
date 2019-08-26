d4b4877e64f1c9cdabe20e55f80a6db3d4e3c7c1
tcstewart95 <tcstewart95@gmail.com>
2019-08-26 11:15:46 -0600

    Updating for publishing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## 0.0.2
 
-* TODO: Describe initial release.
+* This version changes the return types of each function to be more intuitive.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,7 +34,7 @@ class _StepCounterState extends State<AccessSteps> {
 
   readAll() {
     String results = "";
-    Future<Map<String, int>> stepCount = StepCounter.getStepsToday();
+    Future<int> stepCount = StepCounter.getStepsToday();
     stepCount.then((value) {
       results = value.toString();
       setState(() {
